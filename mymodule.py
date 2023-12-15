d379b6bc0bf86ea252185c7933bb69e11421d94f
Aman Yadav <97920298+Amanblackdevil@users.noreply.github.com>
2023-12-15 12:17:08 +0530

    Add files via upload

---
+++ mymodule.py
@@ -0,0 +1,23 @@
+'''
+def greeting(name):
+  print("Hello, " + name)
+'''
+
+'''
+person1 = {
+  "name" : "Aman",
+  "age" : 36,
+  "country" : "India"
+}
+
+'''
+
+
+def person(name):
+  print("Hello ", name)
+
+  person1 = {
+    "name" : "aman",
+    "age" : 36,
+    "country" : "india"
+  }
